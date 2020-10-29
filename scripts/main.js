15c37b63753e00027f906d9fec4ee2892e0ae786
Urszula Dobrowolska <urszulad26@gmail.com>
2020-10-29 13:50:07 -0400

    added suggested Juno changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,39 @@
 
 const app = {};
 
+app.id = '5c79c0b4';
+app.key = '1cca38a8725080f71faedd0f70080646';
+
 app.food = [
     {
     type: "protein",
     varieties: [
-        { name: 'Steak', serving: '3 oz', image: 'assets/icons/protein-beef.svg' },
-        { name: 'chicken', serving: '4 oz', image: 'assets/icons/protein-chicken.svg' },
-        { name: 'shrimp', serving: '4 oz', image: 'assets/icons/protein-shrimp.svg' },
-        { name: 'tofu', serving: '150 grams', image: 'assets/icons/protein-tofu.svg' },
-        { name: 'eggs', serving: "two", image: 'assets/icons/protein-eggs.svg' },
-        { name: 'salmon', serving: '4 oz', image: 'assets/icons/protein-salmon.svg' }
+        { name: 'Steak', serving: '3 oz', image: 'protein-beef.svg' },
+        { name: 'chicken', serving: '4 oz', image: 'protein-chicken.svg' },
+        { name: 'shrimp', serving: '4 oz', image: 'protein-shrimp.svg' },
+        { name: 'tofu', serving: '150 grams', image: 'protein-tofu.svg' },
+        { name: 'eggs', serving: "two", image: 'protein-eggs.svg' },
+        { name: 'salmon', serving: '4 oz', image: 'protein-salmon.svg' }
         ],
     },
     {
     type: "veg",
     varieties: [
-        { name: 'asparagus', serving: '6 medium spears', image: 'assets/icons/veg-asparagus.svg' },
-        { name: 'beets', serving: '1/2 whole medium', image: 'assets/icons/veg-beets.svg' },
-        { name: 'broccoli', serving: '1 cup chopped', image: 'assets/icons/veg-brocolli.svg' },
-        { name: 'carrot', serving: '1 cup chopped', image: 'assets/icons/veg-carrot.svg' },
-        { name: 'zucchini', serving: '1 cup sliced', image: 'assets/icons/veg-zucchini.svg' }
+        { name: 'asparagus', serving: '6 medium spears', image: 'veg-asparagus.svg' },
+        { name: 'beets', serving: '1/2 whole medium', image: 'veg-beets.svg' },
+        { name: 'broccoli', serving: '1 cup chopped', image: 'veg-brocolli.svg' },
+        { name: 'carrot', serving: '1 cup chopped', image: 'veg-carrot.svg' },
+        { name: 'zucchini', serving: '1 cup sliced', image: 'veg-zucchini.svg' }
         ],
     },
     {
     type: "carb",
     varieties: [
-        { name: 'baked potato', serving: 'one medium', image: 'assets/icons/carb-baked-potato.svg' },
-        { name: 'pasta', serving: '1 cup', image: 'assets/icons/carb-pasta-tomato.svg' },
-        { name: 'brown rice', serving: '1/2 cup', image: 'assets/icons/carb-rice-brown.svg' },
-        { name: 'white rice', serving: '1/2 cup', image: 'assets/icons/carb-rice-white.svg' },
-        { name: 'sweet potato', serving: 'one medium', image: 'assets/icons/carb-sweet-potato.svg' }
+        { name: 'baked potato', serving: 'one medium', image: 'carb-baked-potato.svg' },
+        { name: 'pasta', serving: '1 cup', image: 'carb-pasta-tomato.svg' },
+        { name: 'brown rice', serving: '1/2 cup', image: 'carb-rice-brown.svg' },
+        { name: 'white rice', serving: '1/2 cup', image: 'carb-rice-white.svg' },
+        { name: 'sweet potato', serving: 'one medium', image: 'carb-sweet-potato.svg' }
         ]
     }
 ]
@@ -54,12 +57,18 @@ app.pickRandomItem = function(array){
 
 // Add to HTML on click
 app.assignHtml = function(array, id){
-    $(`div#main-img-${id}`).html(`<img src="${array.image}" alt="${array.name}"/>`);
+    $(`div#main-img-${id}`).html(`<img src="assets/icons/${array.image}" alt="${array.name}"/>`);
     $(`#randomizer__info--${id}`).html(`
         <h2 id="result-${id}">${array.name}</h2>
         <h3 id="result-${id}">${array.serving}</h3>
     `)
 };
+
+// Show error if the api does not work
+app.errorMessage = function(){
+    $('.error').removeClass("hidden");
+}
+
 
 
 app.displayApiInfo = (id, apiDataCals, apiDataProts, apiDataFats, apiDataCarbs) =>{ 
@@ -71,7 +80,7 @@ app.displayApiInfo = (id, apiDataCals, apiDataProts, apiDataFats, apiDataCarbs) 
     `)
 }
 
-app.getApiInfo = (id, item) => {
+app.getApiInfo = (id, item = "chicken") => {
     $.ajax({
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
@@ -81,22 +90,20 @@ app.getApiInfo = (id, item) => {
           params: {
             method: 'GET',
             dataType: 'json',
-            app_id: '5c79c0b4',
-            app_key: '1cca38a8725080f71faedd0f70080646',
+            app_id: app.id,
+            app_key: app.key,
             ingr: `${item}`
           }
         }
       }).then(function(results) {
           const apiCalories = results.calories;
-          const apiProtein = (Math.floor(results.totalNutrients.PROCNT.quantity)) + results.totalNutrients.PROCNT.unit;
+          const apiProtein = (Math.round(results.totalNutrients.PROCNT.quantity)) + results.totalNutrients.PROCNT.unit;
           const apiFats = (Math.floor(results.totalNutrients.FAT.quantity)) + results.totalNutrients.FAT.unit;
           const apiCarbs = (Math.floor(results.totalNutrients.CHOCDF.quantity)) + results.totalNutrients.CHOCDF.unit;
 
           app.displayApiInfo(id, apiCalories, apiProtein, apiFats, apiCarbs);
 
-      }).catch(function(error){
-          console.log('it broke', error);
-      })
+      }).catch(app.errorMessage);
     
     };
 
@@ -104,12 +111,17 @@ app.getApiInfo = (id, item) => {
 // Code to kick off the app goes here.  --------------------------------------------------------
 app.init = function() {
     
+    // run the api on page open to have a faste api call.
+    app.getApiInfo(null, "chicken 4 oz"); 
+
     // Find the array associated with the button clicked on
     $('.btn-randomize').on('click', function(){
 
+        $('.error').addClass("hidden");
         const buttonId = $(this).attr('id');
         let foodArray = app.pickRandomItem(app.findFoodArray(buttonId));
         const nextFood = `${foodArray.name} ${foodArray.serving}`;
+        
         
         // whatever item is randomly generated on click will pass into this function so that the api knows what to look for. The api then spits out the item
         app.getApiInfo(buttonId, nextFood); 
